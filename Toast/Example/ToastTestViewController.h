47a6831179ad96f1fd2ed78646716db0cebafff9
Der-Johng Sun <derjohng@doitwell.tw>
2015-05-28 20:14:15 +0800

    init

---
+++ Toast/Example/ToastTestViewController.h
@@ -0,0 +1,12 @@
+//
+//  ToastTestViewController.h
+//  ToastTest
+//
+//  Copyright 2015 Der-Johng Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ToastTestViewController : UIViewController
+
+@end
